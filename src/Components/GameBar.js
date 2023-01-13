afc341c681e9fe5246cbfae324a37c063ab01c3a
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-01-13 13:42:22 -0600

    Initial commit for basic structuring of components

---
+++ src/Components/GameBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GameBar = () => {
+    return (
+        <div className="GameBar">
+            <h2>You are Finding Colby</h2>
+            <span>Timer Go Here</span>
+        </div>
+    );
+};
+
+export default GameBar;
---
+++ src/Components/PlayArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from '../Forsaken-Exotics.jpg'
+
+const PlayArea = () => {
+    return (
+        <div className="PlayArea">
+            <img src={image} alt='Exotics from Forsaken Expansion of Destiny 2' />
+        </div>
+    );
+};
+
+export default PlayArea;
