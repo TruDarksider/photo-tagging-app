d08823d801c58bd751dd29a39deb8559ba5150c0
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-01-20 16:57:17 -0600

    Finished dropdown menu testing and implementation for click creating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import React from 'react';
 import GameBar from './Components/GameBar.js';
-import PlayArea from './Components/PlayArea'
-import { initializeApp } from 'firebase/app'
+import PlayArea from './Components/PlayArea';
+//import DropdownMenu from './Components/DropdownMenu';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc, getDocs } from 'firebase/firestore';
 
 const firebaseConfig= {
   apiKey: "AIzaSyDnPN-s6Tupny3TFCQTLuRlzV6YOQxyNig",
@@ -14,12 +16,24 @@ const firebaseConfig= {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//The Backend call?
+// Get a list of Colbys from your database
+async function getColbyLocation(db) {
+  const colbyCol = collection(db, 'colbys');
+  const colbySnapshot = await getDocs(colbyCol);
+  const colbyList = colbySnapshot.docs.map(doc => doc.data());
+  return colbyList;
+}
 
 function App() {
+  const answerKey = getColbyLocation(db);
   return (
     <div className="App">
       <GameBar />
-      <PlayArea />
+      {/* <DropdownMenu /> */}
+      <PlayArea answerKey={answerKey}/>
     </div>
   );
 }
@@ -30,6 +44,9 @@ export default App;
 Backend needs to have an answer key as to where each character is on image
 -I need to create answer key, but what does the key look like? A set of pixels that is acceptable? Relative position within the picture?
 -And is the key a picture or just a file with a list of possibilities?
+
+--Above may be partially started by calling for the backend answer key.... which doesn't exist yet
+- for reference: https://firebase.google.com/docs/firestore/query-data/get-data
 
 
 
--- src/Components/PlayArea.js
+++ src/Components/PlayArea.js
@@ -1,10 +1,76 @@
-import React from 'react';
-import image from '../Forsaken-Exotics.jpg'
+import React, { useState } from 'react';
+import image from '../ColbysAndCrew.png'
 
 const PlayArea = () => {
+  const [cursor, setCursor] = useState('crosshair');
+  const [guess, setGuess] = useState(false);
+
+  const changeCursor = () => {
+    setCursor(prevState =>{
+      if(prevState === 'crosshair'){
+        return 'pointer'
+      }
+      return 'crosshair';
+    });
+  }
+
+  const handleGuess = () => {
+    setGuess(!guess)
+    document.querySelector('img').onclick = function getCoordinates(e) {
+      let bounds = e.target.getBoundingClientRect();
+      let xCoor = e.clientX - bounds.left;
+      let yCoor = e.clientY - bounds.top;
+      console.log('Left: ' + xCoor + ' Top: ' + yCoor);
+    }
+  }
+
+  function showColbyOptions(e) {
+    //Create Container Div
+    let mouseMenu = document.createElement('div');
+    mouseMenu.setAttribute('id', 'myDropdown');
+    mouseMenu.classList.add('dropdown-content');
+    let x = e.clientX;
+    let y = e.clientY;
+    mouseMenu.style.position = 'fixed';
+    mouseMenu.style.left = x + 'px';
+    mouseMenu.style.top = y + 'px';
+    //Create Menu
+    let colbyOptions = document.createElement('ul');
+    let colby1 = document.createElement('li');
+    colby1.textContent = 'Colby 1';
+    colbyOptions.appendChild(colby1);
+    let colby2 = document.createElement('li');
+    colby2.textContent = 'Colby 2';
+    colbyOptions.appendChild(colby2);
+    let colby3 = document.createElement('li');
+    colby3.textContent = 'Colby 3';
+    colbyOptions.appendChild(colby3);
+    mouseMenu.appendChild(colbyOptions);
+    document.getElementById('PlayArea').appendChild(mouseMenu);
+    document.getElementById('myDropdown').classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+    function removeDropdown(e){
+    if (!e.target.matches('li') ) {
+      document.getElementById('myDropdown').remove();
+    }
+  } 
+
+  const colbyLocationGuess = (e) => {
+    if(document.getElementById('myDropdown')){
+      removeDropdown(e);
+      changeCursor();
+    } else {
+      changeCursor();
+      handleGuess();
+      showColbyOptions(e);
+    }
+  }
+
     return (
-        <div className="PlayArea">
-            <img src={image} alt='Exotics from Forsaken Expansion of Destiny 2' />
+        <div id='PlayArea' className="PlayArea" style={{cursor: cursor}} onClick={colbyLocationGuess}>
+            <img src={image} alt='Character collage using You are Colby artwork' />
         </div>
     );
 };
