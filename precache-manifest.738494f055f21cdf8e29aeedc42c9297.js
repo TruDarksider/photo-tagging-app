ca556b623d805a6863e599ba5b1fa2aeb5ee0144
Tyler Hopkin <tyler.hopkin12@gmail.com>
2023-04-28 12:53:26 -0500

    Updates

---
+++ precache-manifest.738494f055f21cdf8e29aeedc42c9297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e9412833d478ef9aa8f",
+    "url": "/photo-tagging-app/static/css/main.63144782.chunk.css"
+  },
+  {
+    "revision": "0e9412833d478ef9aa8f",
+    "url": "/photo-tagging-app/static/js/main.789f086a.chunk.js"
+  },
+  {
+    "revision": "de0ad41fa299abfee196",
+    "url": "/photo-tagging-app/static/js/runtime~main.6599d81a.js"
+  },
+  {
+    "revision": "60a3282c9eff2d2dcbb2",
+    "url": "/photo-tagging-app/static/js/2.a668410a.chunk.js"
+  },
+  {
+    "revision": "bf332c237624ea94aedb8402a52f1d08",
+    "url": "/photo-tagging-app/index.html"
+  }
+];
